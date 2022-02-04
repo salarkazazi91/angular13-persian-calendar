ffe2d04c5cca67805ad28b145df082d7f3326a2a
salar.kazazi@gmail.com <Salar@DESKTOP-ELE408D>
2022-02-04 19:05:12 +0330

    added persian calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgPersianDatepickerModule } from 'ng-persian-datepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgPersianDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
