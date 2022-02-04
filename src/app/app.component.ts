4e75dfc391b50711fab157b4794b90862f0cd1b1
salar.kazazi@gmail.com <Salar@DESKTOP-ELE408D>
2022-02-04 18:47:48 +0330

    initialized + moment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'jalali-moment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  todayJalali: any;
+  ngOnInit(): void {
+
+    this.todayJalali = moment("2/3/2022").locale('fa').format('YYYY/M/D');
+  }
   title = 'jalali';
 }
